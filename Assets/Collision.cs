50cad18bb7d062d54d3c4008f69b9b76933151b9
RJLawrence1 <lawren610@morrisville.edu>
2025-09-11 09:35:35 -0400

    Added Collision

---
+++ Assets/Collision.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Ouch!");
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("What the heck was that?");
+    }
+}
---
+++ Assets/Driver.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Driver : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 0.05f;
+    [SerializeField] float steerSpeed = 1.5f;
+
+    void Update()
+    {
+        float steer = 0f;
+        float move = 0f;
+
+        if (Keyboard.current.wKey.isPressed)
+        {
+            move = 1f;                                                                                  
+        }
+
+        else if (Keyboard.current.sKey.isPressed)
+        {
+            move = -1f;
+        }
+
+        if (Keyboard.current.aKey.isPressed)
+        {
+            steer = 1f;
+        }
+
+        else if (Keyboard.current.dKey.isPressed)
+        {
+            steer = -1f;
+        }
+
+        float moveAmount = move * moveSpeed * Time.deltaTime;
+        float steerAmount = steer * steerSpeed * Time.deltaTime;
+
+        transform.Translate(0, moveAmount, 0);
+        transform.Rotate(0, 0, steerAmount);
+    }
+}
